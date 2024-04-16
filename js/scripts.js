501be48b7aa187dcf505de81b6cc969618b3c370
Fernando Mata <fercho@Air-de-Fernando.cgocable.net>
2024-04-16 00:05:57 -0400

    added changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@ $(document).ready(function(){
     $('.slider').slick({
         infinite: true,
         slidesToShow: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
         slidesToScroll: 1
     });
   });
@@ -13,6 +15,7 @@ var i;
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
     this.classList.toggle("active");
+    ('parrafo').classList.toggle("activo");
   });
 }
 
